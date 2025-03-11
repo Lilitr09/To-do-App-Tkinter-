1f31beaf061a464e779e5986979e679352c8348a
Lilitr <liliananathaly1409@gmail.com>
2025-03-11 14:53:41 -0400

    Main layout finished

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import scrolledtext
 
 
 def main():
@@ -20,7 +21,7 @@ def main():
 
     # Tasks Entry
     task = Entry(
-        window, bg=background_color, bd=4, fg=text_color, font=("calibre", 13, "bold")
+        window, bg=background_color, bd=4, fg=text_color, font=("calibre", 15, "bold")
     )
     task.pack(fill=X, padx=10, pady=10)
     # Add Task Button
@@ -36,6 +37,26 @@ def main():
         activeforeground="white",
     )
     add_task_button.pack()
+    # Tasks area
+    tasks_area = Listbox(window, bg=background_color, activestyle="dotbox", fg=highlight_color, height=20)
+    tasks_area.pack(fill=X, padx=10, pady=10)
+    # Delete task button
+    delete_task_button = Button(
+        window,
+        text="Delete task",
+        bg=secondary_color,
+        fg=text_color,
+        padx=5,
+        pady=5,
+        cursor="hand2",
+        activebackground=primary_color,
+        activeforeground="white",
+    )
+    delete_task_button.pack()
+    
+    # Footer
+    message = Label(window, text="Made by Liliana Torres")
+    message.pack(padx=10, pady=10)
 
     # App execuition
     window.mainloop()
