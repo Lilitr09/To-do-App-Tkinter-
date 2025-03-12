e2c67ceac5edce5aa8ead6741ee4bfa48888b083
Lilitr <liliananathaly1409@gmail.com>
2025-03-12 14:43:29 -0400

    First version finished

--- main.py
+++ main.py
@@ -1,6 +1,48 @@
 from tkinter import *
 from tkinter import scrolledtext
+from tkinter import messagebox
 from PIL import Image, ImageTk
+
+
+def add_task(entry, list):
+    """Adds a task from the Entry box to the Listbox.
+
+    Retrieves the text entered in the Entry,
+    inserts it into the Listbox,
+    and then clears the Entry.
+
+    Args:
+        entry (Entry): The Entry widget from which to get the task.
+        list (Listbox): The Listbox widget to add the task to.
+
+    Returns:
+        None
+    """
+    task = entry.get()
+    if task:
+        list.insert(END, "• " + task)
+        entry.delete(0, END)
+    else:
+        messagebox.showerror("Error", "Empty task")
+
+
+def delete_task(list):
+    """Deletes the selected task from the Listbox.
+
+    Gets the index of the selected task in the Listbox and deletes it.
+    If no task is selected, shows a warning message.
+
+    Args:
+        list (Listbox): The Listbox widget from which to delete the task.
+
+    Returns:
+        None
+    """
+    try:
+        index = list.curselection()[0]
+        list.delete(index)
+    except IndexError:
+        messagebox.showwarning("Warning", "Please, select a task to delete")
 
 
 def main():
@@ -8,8 +50,8 @@ def main():
     definition"""
     background_color = "#F5F5F5"  # Light grey
     text_color = "#212121"  # Dark grey
-    primary_color = "#3498DB"  # Green
-    secondary_color = "#2980B9"  # Light green
+    primary_color = "#3498DB"  # Blue
+    secondary_color = "#2980B9"  # Dark blue
     highlight_color = "#FFEB3B"  # Yellow
     red_button = "#E74C3C"
     red_button_hover = "#C0392B"
@@ -17,22 +59,22 @@ def main():
     # Window configuration
     window = Tk()
     window.title("To-do App")
-    window.geometry("500x600")
+    window.geometry("500x665")
     window.configure(bg=background_color)
     window.resizable(0, 0)
-    
+
     icon = PhotoImage(file="to-do-icon.png")
     window.iconphoto(False, icon)
 
     # App layout
-    
+
     # Image header
     try:
         image = Image.open("to-do-header.png")
         image_resized = image.resize((150, 90))
         image_tk = ImageTk.PhotoImage(image_resized)
-        
-        label_image = Label(window, image=image_tk)
+
+        label_image = Label(window, image=image_tk, bg=background_color)
         label_image.image = image_tk
         label_image.pack()
     except FileNotFoundError:
@@ -54,11 +96,19 @@ def main():
         cursor="hand2",
         activebackground=secondary_color,
         activeforeground="white",
-        font=("calibre", 10, "bold")
+        font=("calibre", 10, "bold"),
+        command=lambda: add_task(task, tasks_area),
     )
     add_task_button.pack()
     # Tasks area
-    tasks_area = Listbox(window, bg=background_color, activestyle="dotbox", fg=highlight_color, height=20)
+    tasks_area = Listbox(
+        window,
+        bg=background_color,
+        activestyle="dotbox",
+        fg=text_color,
+        height=16,
+        font=("Arial", 15),
+    )
     tasks_area.pack(fill=X, padx=10, pady=10)
     # Delete task button
     delete_task_button = Button(
@@ -71,12 +121,13 @@ def main():
         cursor="hand2",
         activebackground=red_button_hover,
         activeforeground="white",
-        font=("calibre", 10, "bold")
+        font=("calibre", 10, "bold"),
+        command=lambda: delete_task(tasks_area),
     )
     delete_task_button.pack()
-    
+
     # Footer
-    message = Label(window, text="Made by Liliana Torres")
+    message = Label(window, text="Made by Liliana Torres", bg=background_color)
     message.pack(padx=10, pady=10)
 
     # App execuition
